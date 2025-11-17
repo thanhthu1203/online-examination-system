69adf2ba63f5f5580a2ef5dd5767c3eabebcc6b1
thanhthu1203 <ITCSIU24083@student.hcmiu.edu.vn>
2025-11-17 15:49:40 +0700

    Revert "backend"

--- backend_src_main_java_com_yourexam_repository_AdminRepository_Version3.java
+++
@@ -1,7 +0,0 @@
-package com.yourexam.repository;
-
-import com.yourexam.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-}
